f4d21120bd3a69eabc86ad62f6c25cefed03a33f
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-12 15:00:02 +0900

    feat: change impl

--- swim_darwin.go
+++ swim_darwin.go
@@ -2,21 +2,39 @@ package swim
 
 /*
 #cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Foundation -framework Carbon
+#cgo LDFLAGS: -framework Foundation -framework Carbon -framework AppKit
 #import <Foundation/Foundation.h>
 
+void init();
+void eventloop();
 const char* get();
 int set(const char *sourceID);
 */
 import "C"
 import (
 	"errors"
+	"runtime"
 	"unsafe"
 )
 
+func init() {
+	runtime.LockOSThread()
+	C.init()
+}
+
 func Get() (string, error) {
-	cstr := C.get()
-	return C.GoString(cstr), nil
+	ch := make(chan string)
+	go func() {
+		cstr := C.get()
+		ch <- C.GoString(cstr)
+	}()
+	select {
+	case ret := <-ch:
+		return ret, nil
+	default:
+		C.eventloop()
+	}
+	return <-ch, nil
 }
 
 func Set(sourceID string) error {
--- swim_darwin.m
+++ swim_darwin.m
@@ -2,13 +2,61 @@
 
 #import <Foundation/Foundation.h>
 #import <Carbon/Carbon.h>
+#import <AppKit/AppKit.h>
 
-const char* get() {
+@interface InputMethodObserver : NSObject {
+  const char* current;
+  NSTextInputContext *context;
+}
+@end
+
+@implementation InputMethodObserver : NSObject
+- (id) init {
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
+  context = [[NSTextInputContext alloc] initWithClient: [NSTextView new]];
+  [self update: nil];
+  return self;
+}
+
+- (void) update: (NSNotification*) notif {
+  NSLog(@"%@", notif);
   TISInputSourceRef currentInputSource = TISCopyCurrentKeyboardInputSource();
   NSString *sourceID = (__bridge NSString *)(TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID));
-  const char *cstrSourceID = [sourceID UTF8String];
+  current = [sourceID UTF8String];
   CFRelease(currentInputSource);
-  return cstrSourceID;
+  printf("%s\n", current);
+}
+
+- (const char*) current {
+  return current;
+}
+@end
+
+InputMethodObserver *observer;
+
+void init() {
+  observer = [[InputMethodObserver alloc] init];
+  [[NSDistributedNotificationCenter defaultCenter]
+    addObserver: observer
+       selector: @selector(update:)
+           name: (__bridge NSString*)kTISNotifySelectedKeyboardInputSourceChanged
+         object: nil
+     suspensionBehavior: NSNotificationSuspensionBehaviorDeliverImmediately
+  ];
+}
+
+void eventloop() {
+    @autoreleasepool {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+}
+
+const char* get() {
+  return [observer current];
 }
 
 int set(const char *sourceID) {
